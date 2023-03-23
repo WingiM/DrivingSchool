7b6381dffeec31e04cdf3122fae0ba23d72ce268
Kamil Sharipov <wingimobile@gmail.com>
2023-03-23 11:02:04 +0300

    convert hours to academic

---
+++ DrivingSchool.Domain/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace DrivingSchool.Domain.Extensions;
+
+public static class TimeSpanExtensions
+{
+    public static TimeSpan ToAcademicHours(this TimeSpan span)
+    {
+        return TimeSpan.FromHours(span.TotalHours * 0.75);
+    }
+}
