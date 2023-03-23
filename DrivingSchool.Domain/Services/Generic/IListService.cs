5677a5db7c7cfb0fa4df8df10d62e93ede3e5280
Камиль Шарипов <wingimobile@gmail.com>
2023-03-23 21:48:38 +0300

    add generic component to display list items

---
+++ DrivingSchool.Domain/Services/Generic/IListService.cs
@@ -0,0 +1,6 @@
+﻿namespace DrivingSchool.Domain.Services.Generic;
+
+public interface IListService<T> where T : Entity
+{
+    public Task<ListDataResult<T>> ListAsync(int itemCount, int pageNumber, Predicate<T>? filter = null);
+}
